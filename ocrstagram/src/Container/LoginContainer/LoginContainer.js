2e4a7e8faf5f25b063f05c9d523c8562e2d3a8ba
sangmeeeee <goodman1223@naver.com>
2021-05-29 18:17:52 +0900

    axios login

--- ocrstagram/src/Container/LoginContainer/LoginContainer.js
+++ ocrstagram/src/Container/LoginContainer/LoginContainer.js
@@ -1,18 +1,55 @@
+import axios from 'axios'
 import React from 'react'
+import { Router } from 'react-router'
 import './LoginContainer.css'
+const url = 'http://localhost:8080/'
 
 class LoginContainer extends React.Component{
+    constructor(props) {
+        super(props)
+        this.state = {
+            ID : null,
+            PW : null
+        }
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        axios.post(url,{
+            id : this.state.ID,
+            pw : this.state.PW
+        }).then(() => {
+            window.location.href+="user"
+        }).catch(() => {
+            console.error('error in axios')
+        })
+    }
+
+    handleID = (e) => {
+        this.setState({ ID : e.target.value });
+        e.preventDefault()
+    }
+
+    handlePW = (e) => {
+        this.setState({ PW : e.target.value });
+        e.preventDefault()
+    }
+
     render(){
         return(
             <div className="LoginContainer">
-                <form>
-                    <p>ID</p>
-                    {/* ID */}
-                    <p>Password</p>
-                    {/* PassWord */}
+                <form className="LoginForm" onSubmit={this.handleSubmit}>
+                    <p>
+                        <input type="text" name="ID" placeholder="ID" onChange={this.handleID}></input>
+                    </p>
+                    <p>
+                        <input type="password" name="PW" placeholder="PW" onChange={this.handlePW}></input>
+                    </p>
+                    <p>
+                        <input type="submit" value="Sign in"/>
+                    </p>
                 </form>
-                    <p><a href="/user">Sign in</a></p>
-                    <p><a href="/signup">Sign up</a></p>
+                    <p><button onClick={window.location.href += "signup"}>Sign up</button></p>
             </div>
         )
     }
