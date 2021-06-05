a5f2aa98fbbc61f3657ca155964c9e5f1a903e9d
dohkim <dh54kim54@gmail.com>
2021-06-05 16:22:00 +0900

    refactoring

---
+++ ocrstagram/servers/controllers/imgOcr.js
@@ -0,0 +1,31 @@
+const { createWorker } = require('tesseract.js');
+const asyncHandler = require('express-async-handler');
+
+module.exports = asyncHandler(async (req, res, next) => {
+    let img = req.files.img
+
+    let imgPath = path.resolve(__dirname,'..',`public/img/${req.params.id}`,img.name);
+    try {
+        if (fs.existsSync(imgPath)) {
+          res.send("Same filename exists")
+        }
+        else {
+            img.mv(imgPath, async(err) => {
+                console.log('upload img to server')
+            })
+
+            const worker = createWorker();
+            await worker.load();
+            await worker.loadLanguage('eng');
+            await worker.initialize('eng');
+            // ocr img file
+            const { data: { text } } = await worker.recognize(imgPath);
+            console.log(text);
+            // send ocr text
+            res.send('upload img to server\n' + text);
+            await worker.terminate();
+        }
+      } catch(err) {
+        console.error(err)
+    }
+})
