34e556db455671707edd93b15b6c24134c03d011
sangmeeeee <goodman1223@naver.com>
2021-06-05 16:21:12 +0900

    add register user

--- ocrstagram/src/Container/SignupContainer/SignupContainer.js
+++ ocrstagram/src/Container/SignupContainer/SignupContainer.js
@@ -1,9 +1,41 @@
 import './SignupContainer.css'
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
 import { Button, Form, Grid, Header, Image, Message, Segment, Icon } from 'semantic-ui-react'
 
 class SignupContainer extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      id : null,
+      pw : null
+    }
+  }
+
+  onChangeId = (event) => {
+    event.preventDefault()
+    this.setState({id : event.target.value})
+  }
+
+  onChangePw = (event) => {
+    event.preventDefault()
+    this.setState({pw : event.target.value})
+  }
+
+  onSubmitHandler = (event) => {
+    event.preventDefault()
+    axios.post(`http://localhost:8080/user/register`,{
+      id : this.state.id,
+      pw : this.state.pw
+    }).then((result) => {
+      console.log(result.data)
+      window.location.href='/'
+    }).catch((err) => {
+      console.error(err)
+    })
+  }
+
   render() {
     return (
       <Grid id="signUpGrid" textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
@@ -11,12 +43,12 @@ class SignupContainer extends React.Component {
           <Header as='h2' color='teal' textAlign='center'>
             {/* <Image id="logo" src='./images/ocrstagram.png' /> */}
           </Header>
-          <Form size='large'>
+          <Form size='large' onSubmit={this.onSubmitHandler}>
             <Segment stacked>
-              <Form.Input fluid icon='circle' iconPosition='left' placeholder='Name' required='true' />
-              <Form.Input fluid icon='phone' iconPosition='left' placeholder='Phone' required='true' />
-              <Form.Input fluid icon='user' iconPosition='left' placeholder='ID' required='true' />
+              <Form.Input onChange={this.onChangeId.bind(this)}    className='id' fluid icon='user' iconPosition='left' placeholder='ID' required='true' />
               <Form.Input
+              onChange={this.onChangePw.bind(this)}
+                className='pw'
                 fluid
                 icon='lock'
                 iconPosition='left'
