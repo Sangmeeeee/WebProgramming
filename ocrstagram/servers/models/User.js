17d5591bcca4d66fd85f07c4b0af0b18f2be8e1d
sangjune ahn <twknds@naver.com>
2021-06-05 16:50:15 +0900

    server_update

---
+++ ocrstagram/servers/models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+const bcrypt = require('bcryptjs')
+ 
+const UserSchema = new Schema({  
+    userid: { // pass in config object. and put in validation rules 
+      type: String,
+      required: true,
+      unique: true 
+    },
+    pw: {
+      type: String,
+      required: true
+    }
+  });
+
+
+// note: no lambda func! (not work!)
+// UserSchema.pre('save', function(next){
+//     const user = this      
+//     bcrypt.hash(user.pw, 10,  (error, hash) => {        
+//       user.password = hash 
+//       next() 
+//     }); 
+// });
+
+const User = mongoose.model('User',UserSchema);
+module.exports = User
