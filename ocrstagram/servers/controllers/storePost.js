b3ea81ce49e05e18896850b1310220036d1f9eaf
sangmeeeee <goodman1223@naver.com>
2021-06-06 03:47:47 +0900

    remove-duplicate

--- ocrstagram/servers/controllers/storePost.js
+++ ocrstagram/servers/controllers/storePost.js
@@ -1,18 +1,24 @@
 const Post = require('../models/Post')
 const User = require('../models/User')
+const path = require('path')
+const fs = require('fs')
 
-module.exports = (req, res, next) => { // id랑 파일이 같이옴
-    let image = req.files.img
+module.exports = async (req, res, next) => { // id랑 파일이 같이옴
+    let img = req.files.img
     let id = req.params.id
 
-    User.findOne({userid: id}, (error, user) => {
-        let _id = user._id
-        console.log(_id)
-        Post.create({
-            text:req.body.text,
-            image : '/img/' + image.name,
-            username : _id
+    let temp = new Date().getTime().toString()
+    let imgPath = path.resolve(__dirname,'..','..',`public/img/${req.params.id}`,temp+'.jpg')
+
+    img.mv(imgPath, async(err) => {
+        User.findOne({userid: id}, (error, user) => {
+            let _id = user._id
+            Post.create({
+                text:req.body.text,
+                image : '/img/' + temp,
+                username : _id
+            })
+            res.send('ok')
         })
-        res.send('ok')
     })
 }
