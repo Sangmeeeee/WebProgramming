b3afb6f8ff548fbd7c6e1e4fcefbd44c8f3f8406
sangmeeeee <goodman1223@naver.com>
2021-06-07 00:49:07 +0900

    add Serach Input to Header

--- ocrstagram/src/Components/Header/Header.js
+++ ocrstagram/src/Components/Header/Header.js
@@ -1,16 +1,19 @@
 import React from 'react'
-import { Icon } from 'semantic-ui-react'
+import { Input,Icon,Container } from 'semantic-ui-react'
 import './Header.css'
 
 class Header extends React.Component {
   render() {
     return (
       <div className="Header">
-        <span>
+        <span>  
           <a href="/">
             <img src="/images/ocrstagram.png" height="100%"></img>
           </a>
         </span>
+        <Container textAlign='center'>
+          <Input icon='search' placeholder='Search...'/>
+        </Container>
       </div>
     )
   }
