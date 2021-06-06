5090b73633754ba1291343b91895903e1432e430
sangmeeeee <goodman1223@naver.com>
2021-06-07 00:48:57 +0900

    not use UserInfo

--- ocrstagram/src/Components/UserInfo/UserInfo.js
+++ ocrstagram/src/Components/UserInfo/UserInfo.js
@@ -1,27 +1,31 @@
-import React from 'react'
-import {Posting} from '../index'
-import cookie from 'react-cookies'
-import './UserInfo.css'
+// import React from 'react'
+// import {Posting} from '../index'
+// import cookie from 'react-cookies'
+// import './UserInfo.css'
 
-class UserInfo extends React.Component{
-    constructor(props){
-        super(props)
-    }
+// class UserInfo extends React.Component{
+//     constructor(props){
+//         super(props)
+//     }
 
-    handlePosting = (e) => {
-        e.preventDefault()
-        document.getElementsByClassName('Posting')[0].style.visibility = 'visible'
-    }
+//     handlePosting = (e) => {
+//         e.preventDefault()
+//         // document.getElementsByClassName('description')[0].innerHTML = '' 
+//         document.getElementsByClassName('uploadImg')[0].src = ''
+//         document.getElementsByClassName('uploadImg')[0].style.width = '0%'
+//         document.getElementsByClassName('uploadImg')[0].style.height = '0%'
+//         document.getElementsByClassName('Posting')[0].style.visibility = 'visible'
+//     }
 
-    render(){
-        return(
-            <div className='UserInfo'>
-                <img className='userProfile'></img>
-                <p>{this.props.id}</p>
-                <button onClick={this.handlePosting}>+</button>
-            </div>
-        )
-    }
-}
+//     render(){
+//         return(
+//             <div className='UserInfo'>
+//                 <img className='userProfile'></img>
+//                 <p>{this.props.id}</p>
+//                 <button onClick={this.handlePosting}>+</button>
+//             </div>
+//         )
+//     }
+// }
 
-export default UserInfo
+// export default UserInfo
