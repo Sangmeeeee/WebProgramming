5710ccf893c78541bcd4dd84b856358b5bef38d9
sangmeeeee <goodman1223@naver.com>
2021-06-10 20:31:11 +0900

    UserInfo로 분리

--- ocrstagram/src/Container/MainContainer/MainContainer.js
+++ ocrstagram/src/Container/MainContainer/MainContainer.js
@@ -6,8 +6,17 @@ class MainContainer extends React.Component{
     render(){
         return(
             <div className='MainContainer'>
-                <img src="/images/mainImg.jpg" style={{'height':'100%', 'width':'50%', float:'left'}}></img>
-                <a href='/user/login'><h1>시작하기</h1></a>
+                <div className='linear'></div>
+                <img src="/images/mainImg.jpg" className='mainImg'></img>
+                <div className='textMain'>
+                    <a href='/user/login'>
+                        <img  className='textImg' src='/images/ocrstagram.png'></img>
+                    </a>
+                    <p>
+                        <strong>이미지를 자유롭게 변환해 보세요</strong>
+                    </p>
+                    <a href='/user/login'><h1>시작하기</h1></a>
+                </div>
             </div>
         )}
 }
--- ocrstagram/src/Container/UserContainer/UserContainer.js
+++ ocrstagram/src/Container/UserContainer/UserContainer.js
@@ -13,60 +13,14 @@ class UserContainer extends React.Component{
     }
 
     render(){
-        if(this.props.match.params.id === this.state.id)
             return(
                 // 나중에 UserInfo로 따로 나누기
                 <div className="UserContainer">
-                    <Container textAlign='right'>
-                        <Button color='black' onClick={
-                            () => document.getElementsByClassName('Posting')[0].style.visibility='visible'
-                            }  animated='fade'>
-                            <Button.Content visible>+</Button.Content>
-                            <Button.Content hidden> Posting </Button.Content>
-                        </Button>
-                    </Container>
-                    <Container textAlign='left'>
-                        <Header as='h2'>
-                            <Image circular src='/images/avatar/large/patrick.png' /> {this.props.match.params.id}
-                        </Header>
-                    </Container>
-                    <Divider />
-
+                    <UserInfo props={this.props} id={this.state.id}/>
                     <p>Posted img area</p>
-
                     <Posting props={this.props} id={this.state.id}/>
                 </div>
-            )
-        else
-            return(
-                <div className="UserContainer">
-                    <Container textAlign='right'>
-                        <Button color='black' disabled='true' onClick={() => {
-                            document.getElementsByClassName('Posting')[0].style.visibility= 'visible'
-                            document.getElementsByClassName('description')[0].innerHTML = '' 
-                            document.getElementsByClassName('uploadImg')[0].src = ''
-                            document.getElementsByClassName('uploadImg')[0].style.width = '0%'
-                            document.getElementsByClassName('uploadImg')[0].style.height = '0%'
-                    }}  animated='fade'>
-                            <Button.Content visible>+</Button.Content>
-                            <Button.Content hidden> Posting </Button.Content>
-                        </Button>
-                    </Container>
-
-                    <Container textAlign='left'>
-                        <Header as='h2'>
-                            <Image circular src='/images/avatar/large/patrick.png' /> {this.props.match.params.id}
-                        </Header>
-                    </Container>
-                    
-                    <Divider />
-
-                    <p>Posted img area</p>
-
-                    <Posting props={this.props} id={this.state.id}/>
-                </div>
-            )
-    }
+            )}
 }
 
 export default UserContainer
