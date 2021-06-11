6271b304308caf96f1e27c194524999ed5e843e6
sangjune ahn <twknds@naver.com>
2021-06-11 20:33:28 +0900

    hash update

--- ocrstagram/servers/controllers/loginUser.js
+++ ocrstagram/servers/controllers/loginUser.js
@@ -1,26 +1,27 @@
 const fs = require('fs')
 const path = require('path')
 const User = require('../models/User')
+const bcrypt = require('bcryptjs')
 
 module.exports = (req, res, next) => {
-
+    
     User.findOne({ userid: req.body.id }, (error, user) => {
-        console.log(user)
         if (user) {
             console.log(req.body)
-            if (req.body.pw === user.pw) {
-
-                res.send(req.body.id)
-                if (!fs.existsSync(`${path.resolve(__dirname, '..', '..', 'public/img')}/${req.body.id}`)) {
-                    console.log('create folder')
-                    fs.mkdirSync(`${path.resolve(__dirname, '..', '..', 'public/img')}/${req.body.id}`)
-
+            bcrypt.compare(req.body.pw, user.pw, (error, same) => {
+                console.log(same)
+                if (same) {
+                    res.send(req.body.id)
+                    if (!fs.existsSync(`${path.resolve(__dirname, '..', '..', 'public/img')}/${req.body.id}`)) {
+                        console.log('create folder')
+                        fs.mkdirSync(`${path.resolve(__dirname, '..', '..', 'public/img')}/${req.body.id}`)
+                        
+                    }
                 }
-            }
-            else {
-                res.redirect('/user/login')
-            }
-
+                else {
+                    res.redirect('/user/login')
+                }
+            })
         }
         else {
             res.redirect('/user/login')
