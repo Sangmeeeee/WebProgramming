3d890116c26eeb389163b0605a333ce4d241481b
sangjune ahn <twknds@naver.com>
2021-06-11 20:22:41 +0900

    update

--- ocrstagram/servers/controllers/loginUser.js
+++ ocrstagram/servers/controllers/loginUser.js
@@ -1,11 +1,32 @@
 const fs = require('fs')
 const path = require('path')
+const User = require('../models/User')
 
 module.exports = (req, res, next) => {
-    res.send(req.body.id)
+
+    User.findOne({ userid: req.body.id }, (error, user) => {
+        console.log(user)
+        if (user) {
+            console.log(req.body)
+            if (req.body.pw === user.pw) {
+
+                res.send(req.body.id)
+                if (!fs.existsSync(`${path.resolve(__dirname, '..', '..', 'public/img')}/${req.body.id}`)) {
+                    console.log('create folder')
+                    fs.mkdirSync(`${path.resolve(__dirname, '..', '..', 'public/img')}/${req.body.id}`)
+
+                }
+            }
+            else {
+                res.redirect('/user/login')
+            }
+
+        }
+        else {
+            res.redirect('/user/login')
+
+        }
+    })
     // console.log(`${path.resolve(__dirname,'..','..','public/img')}/${req.body.id}`)
-    if(!fs.existsSync(`${path.resolve(__dirname,'..','..','public/img')}/${req.body.id}`)){
-        console.log('create folder')
-        fs.mkdirSync(`${path.resolve(__dirname,'..','..','public/img')}/${req.body.id}`)
-    }
+
 }
